4f8ade1c482724a3350514435c5f55514bd19abe
BIT504 <Kiwitiendatm@gmail.com>
2024-07-15 16:53:59 +1200

    hasWon() index error fixed

--- TicTacToe/src/Board.java
+++ TicTacToe/src/Board.java
@@ -54,7 +54,7 @@ public class Board {
 		
 		 // TODO: Check if the player has 3 in the playerCol.
 		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
-		if(cells[playerCol][0].content == thePlayer && cells[playerCol][2].content == thePlayer && cells[playerCol][3].content == thePlayer)
+		if(cells[0][playerCol].content == thePlayer && cells[1][playerCol].content == thePlayer && cells[2][playerCol].content == thePlayer)
 			return true;
 		
 		
